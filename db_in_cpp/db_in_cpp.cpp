b8b62cf1ca130bb9d4b51a7bc59396afb4848fdf
AlexanderBirg <alexander.birg@yandex.ru>
2022-06-29 15:21:47 +0400

    Добавьте файлы проекта.

---
+++ db_in_cpp/db_in_cpp.cpp
@@ -0,0 +1,14 @@
+﻿#include <iostream>
+#include <windows.h>
+
+using namespace std;
+
+int main()
+{
+    SetConsoleCP(1251);
+    SetConsoleOutputCP(1251);
+
+    cout << "Привет, мир!\n";
+
+    return 0;
+}
